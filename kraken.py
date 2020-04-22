104c67fc9052b1beef81d4c0bcc148c42842edbc
Marat <regpo55555@gmail.com>
2020-04-22 22:55:46 +0600

    Moved to venv

---
+++ kraken.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world'
+
+@app.route('/download/<path:url>')
+def download(url):
+    myfile = requests.get(url, allow_redirects=True)
+    content = myfile.headers.get('Content-Disposition')
+    print('content is ', content)
+    pre_filename = content[22:]
+    filename = pre_filename[:-1]
+    print(myfile.headers.get('Content-Disposition'))
+    open(filename, 'wb').write(myfile.content)
+    return send_file(filename, mimetype='application/zip', as_attachment=True, attachment_filename=filename)
+
+@app.route('/download/test')
+def testdownload():
+    filename = 'test.mp4'
+    return send_file(filename, as_attachment=True, attachment_filename=filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
